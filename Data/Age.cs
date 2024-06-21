83a54e3507b6cf763ccb82f02d3e4d980478ac30
TheNameless15 <qnhuy213@gmail.com>
2024-06-21 23:08:20 +0700

    Change in Age Name and Validation of Age

--- Data/Age.cs
+++ Data/Age.cs
@@ -1,16 +1,22 @@
-﻿namespace BlazorApp5.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp5.Data
 {
     public class Age
     {
+        [Required]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Name must be a number")]
         public int Id { get; set; }
-        public string Name { get; set; }
+        
+        public int Name { get; set; }
+
 
         public Age()
         {
 
         }
 
-        public Age(int id, string name)
+        public Age(int id, int name)
         {
             Id = id;
             Name = name;
--- Data/Movie.cs
+++ Data/Movie.cs
@@ -1,38 +1,49 @@
-﻿namespace BlazorApp5.Data
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace BlazorApp5.Data
 {
     public class Movie
     {
         public int Id { get; set; }
-        public  string Title { get; set; }
-        public  string Director { get; set; }
-        public  string Description { get; set; }
-        public  string Image { get; set; }
-       /* public  TimeSpan Duration { get; set; }
-        public  DateTime ReleaseDate { get; set; }*/
-        public  string Language { get; set; }
-/*        public required float Price { get; set; }
-*/       /* public required string IsFeatured { get; set; }*/
-        public  int Status { get; set; }
 
-        public int AgeId {  get; set; }
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(64, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 64 characters")]
+        [RegularExpression(@"^(?!^[0-9]+$)[^<>]+$", ErrorMessage = "Title cannot be a single number and cannot contain '<' or '>' characters")]
+        public string Title { get; set; }
 
+        [Required(ErrorMessage = "Director is required")]
+        [StringLength(64, MinimumLength = 2, ErrorMessage = "Director name must be between 2 and 64 characters")]
+        [RegularExpression(@"^[^0-9]+$", ErrorMessage = "Director name cannot be a number")]
+        public string Director { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public string Language { get; set; }
+
+        public int Status { get; set; } // Assuming validation for Status is handled elsewhere
+
+        public int AgeId { get; set; }
+
+        [Required(ErrorMessage = "Age name is required")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Age name must be a number")]
         public string AgeName { get; set; }
+
         public Movie()
         {
 
         }
 
-        public Movie(int id, string title, 
-            string director, string description, 
-            string image, string language, 
-            int status, int age_id)
+        public Movie(int id, string title, string director, string description, string image, string language, int status, int age_id)
         {
             Id = id;
             Title = title;
             Director = director;
             Description = description;
             Image = image;
-          
             Language = language;
             Status = status;
             AgeId = age_id;
